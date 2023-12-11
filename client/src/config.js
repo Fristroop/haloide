aa8aba5c147ae6b0868cdded9ea96557eba8eae0
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2023-12-11 13:59:30 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,2 @@
-export const API = import.meta.env.PROD
-  ? process.env.VITE_API
-  : "http://localhost:3000";
-
-export const CDN = "https://cdn.fristroop.com/";
+export const API = import.meta.env.VITE_API || "http://localhost:3000";
 console.log(API);
